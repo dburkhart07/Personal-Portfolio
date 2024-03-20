7154e47d151dbabc75b6bfc437a3f7105b08d336
Dalton Burkhart <137221425+dburkhart07@users.noreply.github.com>
2024-03-20 02:08:30 -0400

    Add files via upload

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,32 @@
+import './index.scss'
 
+const About = () => {
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>About me</h1>
+                <p>
+                    WPI CS 2027.
+                    I enjoy doing many aspects of full-stack development. 
+                    My primary focus is in AI and machine learning, as well
+                    as its applications to the healthcare industry. I also like
+                    to pursue creating video games on the side, and am currently
+                    working on a long-term 2D RPG project.
+                </p>
+                <p>
+                    I take pride in my ability to accomplish what I put my mind to,
+                    but I am also humble and always eager to learn new things. 
+                    When it comes to why I am pursuing computer science, my sole
+                    answer is to create technology that will enhance the lives of
+                    others, both locally and globally.
+                </p>
+                <p>
+                    In my free-time, I enjoy playing games with my friends,
+                    trying new foods, and being active.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
