923de9b898f01490afc5e908490aae7c8a6d224a
dburkhart07 <daltonburkhart.75@gmail.com>
2024-11-27 19:15:36 -0500

    Fixed allowed origins

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://dalton-burkhart.onrender.com',
+  'https://dalton-burkhart.onrender.com/',
 ];
 
 app.use(cors({
