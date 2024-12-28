5c5edc4eee24386385045c291c85e215701c0d4f
dburkhart07 <daltonburkhart.75@gmail.com>
2024-12-28 12:43:26 -0500

    Updated footer for delays in database, and updated skills in icon cloud

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-[rgb(102,34,0)] text-[rgb(255,233,209)] py-4 text-center">
-      <p>&copy; {new Date().getFullYear()} DALTON BURKHART. All Rights Reserved.</p>
+      <p>&copy; {new Date().getFullYear()} DALTON BURKHART. All Rights Reserved. Some information may take time to load (upwards of a minute) :)</p>
     </footer>
   );
 }
--- frontend/src/Components/Reusables/IconCloudDemo.jsx
+++ frontend/src/Components/Reusables/IconCloudDemo.jsx
@@ -19,6 +19,7 @@ const slugs = [
   "nextdotjs",
   "tailwindcss",
   "mongodb",
+  "postgres",
   "postman",
   "pytorch",
   "tensorflow",
