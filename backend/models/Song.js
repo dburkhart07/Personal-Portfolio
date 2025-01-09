edcc629967cdb03080ee7ec68b8118821399bff2
dburkhart07 <daltonburkhart.75@gmail.com>
2025-01-08 19:53:57 -0500

    Initial commit for static website

--- backend/models/Song.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const songSchema = new mongoose.Schema({
-  embedUrl: { type: String, required: true },
-  delay: { type: Number, required: true },
-}, { collection: 'Songs' });
-
-module.exports = mongoose.model("Song", songSchema);
