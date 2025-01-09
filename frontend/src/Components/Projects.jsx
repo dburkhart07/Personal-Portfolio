8b56fcb2cc1d55b2f70915e44b6d9738fda641fc
dburkhart07 <daltonburkhart.75@gmail.com>
2025-01-08 20:45:59 -0500

    Final commit for my Website

--- frontend/src/Components/Projects.jsx
+++ frontend/src/Components/Projects.jsx
@@ -11,21 +11,21 @@ const projects = [
   },
   {
       title: "Life Expectancy Predictor",
-      image: "https://img.freepik.com/free-vector/stopwatch-timer-cartoon-icon-illustration_138676-2826.jpg",
+      image: "https://img.freepik.com/free-vector/stopwatch-timer-cartoon-icon-illustration_138676-2420.jpg?t=st=1732763082~exp=1732766682~hmac=17c95b78854ef636fd26912b6aabdedb576500a3996cb09471e44ca16ff5c7a3&w=826",
       techStack: "Python, Scikit-learn, TensorFlow, Pandas, NumPy",
       projectLink: "https://github.com/dburkhart07/Life-Expectancy-Predictor",
       delay: 1
   },
   {
       title: "Project Lilac",
-      image: "https://img.freepik.com/free-photo/arcade-game-world-set-pixelated-retro-game-8bit-digital_53876-131171.jpg",
+      image: "https://img.freepik.com/free-photo/arcade-game-world-set-pixelated-retro-game-world-vector-illustration_1142-50896.jpg?t=st=1732763365~exp=1732766965~hmac=a211dd6faffe664783ce86c5af79e566e681014c12064a706566670582763356&w=826",
       techStack: "C#, Unity",
       projectLink: "https://github.com/Watcher008/Project-Lilac",
       delay: 1.5
   },
   {
       title: "Cancerous Tumor Detector",
-      image: "https://img.freepik.com/free-photo/medical-background-with-abstract-visualization-dna-strand_23-2149198819.jpg",
+      image: "https://img.freepik.com/free-photo/medical-background-with-abstract-virus-cell_1048-14258.jpg?t=st=1732763654~exp=1732767254~hmac=0263d711589dde216acc62ec4979555e711e9a99cc2c044c18e90fe0e90fc214&w=996",
       techStack: "Python, Scikit-learn",
       projectLink: "https://github.com/dburkhart07/Cancerous-tumor-detector",
       delay: 2
